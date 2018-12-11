033decd6f347fb2e785faa1f6559dd8e7703b1eb
ClovisGauzy <imaginarium@clovisgauzy.fr>
2018-12-11 20:10:01 +0100

    Updated: return 404 if `ID` not found

--- fiche.php
+++ fiche.php
@@ -5,28 +5,34 @@
     'banniere' => 'small'
   ];
 
-  // Load Top of Page
-  require_once('./src/templates/html_header.html');
-?>
 
-      <?php
-      if ($_GET AND $_GET['id']) $reqID = $_GET['id'];
+  if ($_GET AND $_GET['id']) $reqID = $_GET['id'];
 
-      if (isset($reqID) AND file_exists('./txt/' . $reqID . '.txt')) {
-        $tableau = yaml_parse_file('./txt/' . $reqID . '.txt');
-        require('./src/templates/doll.html');
-      } else {
-        // throw 404: do send header before content /!\
-        ?>
-        <section>
-          <header>
-            <h2>Introuvable</h2>
-            <h3>Erreur 404</h3>
-          </header>
-          <p>La fiche demandée n'existe pas.</p>
-        </section>
-        <?php
-      }
+  if (isset($reqID) AND file_exists('./txt/' . $reqID . '.txt')) {
+    $tableau = yaml_parse_file('./txt/' . $reqID . '.txt');
+
+    // Load Top of Page
+    require_once('./src/templates/html_header.html');
+
+    // Load Resource
+    require('./src/templates/doll.html');
+  } else {
+
+    // Return HTTP 404 Not Found
+    http_response_code(404);
+
+    // Load Top of Page
+    require_once('./src/templates/html_header.html');
+    ?>
+    <section>
+      <header>
+        <h2>Introuvable</h2>
+        <h3>Erreur 404</h3>
+      </header>
+      <p>La fiche demandée n'existe pas.</p>
+    </section>
+    <?php
+  }
 
   // Load Bottom of Page
   require_once('./src/templates/html_header.html');
