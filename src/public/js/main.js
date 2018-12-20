77e78b277c7b0ce454db6f235810b6f886f0a083
ClovisGauzy <imaginarium@clovisgauzy.fr>
2018-12-20 15:38:00 +0100

    Addition: add new Doll from search results
    + Updated: general design

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -61,7 +61,89 @@ if (table) {
       localStorage.setItem('view', elem.value)
     })
   })
+}
+if (document.querySelector('article.collection')) {
+  let foot = document.createElement('footer')
+  let add = document.createElement('a')
+  add.href = '#add'
+  add.innerHTML ='<span>Ajouter</span>'
 
+  add.addEventListener('click', ev => {
+    ev.preventDefault()
+    add.setAttribute('style', 'display:none;')
+    document.querySelector('body > header').className = 'small'
+    let collection = document.getElementById('collection')
+    let tools = document.getElementById('tools')
+
+    let create = document.createElement('form')
+    create.id = 'create'
+
+    let search = document.createElement('input')
+    search.type = 'search'
+    search.name = 'search'
+    search.setAttribute('placeholder', 'Rechercher')
+
+    let ol = document.createElement('ol')
+    ol.className = 'results'
+    create.innerHTML = `<ol class="breadcrumb">
+    <li><a href="./"><span>Collection</span></a></li>
+    </ol>`
+    create.appendChild(search)
+    create.appendChild(ol) //<ol id="results"></ol>`
+    collection.setAttribute('style','display:none;')
+    tools.setAttribute('style','display:none;')
+    document.querySelector('article.collection').appendChild(create)
+    search.addEventListener('input', ev => {
+      let query = ev.target.value
+      if (query.length > 3) {
+        fetch('https://www.funko.com/ui-api/search?text=' + query, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(results => {
+          if (results.ok && results.status === 200) {
+            return results.json()
+          }
+          return false
+        })
+        .then(results => {
+          if (results && results.products) {
+            ol.innerHTML = ''
+            results.products.forEach((product, i) => {
+              console.log('product', i, product)
+              if (/^Pop!/i.test(product.title)) {
+                let li = document.createElement('li')
+                let a = document.createElement('a')
+                a.href = '#add'
+                a.innerHTML = `<span>Ajouter</span>`
+                li.innerHTML = `<img src="${product.image.src}"/><span>${product.title}</span>`
+                a.remove()
+                li.appendChild(a)
+                a.addEventListener('click', ev => {
+                  ev.preventDefault()
+                  fetch('', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                      handle: product.handle
+                    }),
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                  })
+                  .then(() => tools.querySelector('form').submit())
+                })
+                ol.appendChild(li)
+              }
+            })
+          }
+        })
+      }
+    })
+  })
+  foot.appendChild(add)
+  document.querySelector('article.collection').appendChild(foot)
 }
 
 /// Resource / Doll
